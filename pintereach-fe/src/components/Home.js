b36a2ddea8d7658defb5790625f9bee972a17b0c
jacquetta <jacquettavb@hotmail.com>
2019-07-14 20:19:14 -0500

    home page updated

--- pintereach-fe/src/components/Home.js
+++ pintereach-fe/src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../css/home.css';
+import '../css/bootstrap.min.css';
 
 const Home = () => {
     return (
@@ -10,17 +12,20 @@ const Home = () => {
           to read later. Pintereach helps you research by enabling you to
           save and organize articles in to categories to read later.
         </p>
-        <p>Pintereach is easy to use. Just three simple steps:</p>
+        <div className="container-fluid" id="middle">
+        
+        </div>
+        <p className="home-p">Pintereach is easy to use. Just three simple steps:</p>
         <ol>
           <li>Sign up</li>
           <li>Create and name board</li>
           <li>Post article title or links to board</li>
         </ol>
-        <div>
-          <button>
+        <div className="home-btn">
+          <button type="submit" className="btn btn-dark">
             <a href="./Register">Sign up</a>
           </button>
-          <button>
+          <button type="submit" className="btn btn-dark btn-home">
             <a href="./SignIn">Sign in</a>
           </button>
         </div>
